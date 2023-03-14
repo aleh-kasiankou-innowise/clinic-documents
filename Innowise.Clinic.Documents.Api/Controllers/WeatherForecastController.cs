f304487d1b3dbefb3d9b8b012f0bd7493a2a3643
Aleh Kasiankou <oleg.kosenkov@innowise-group.com>
2023-03-14 09:09:02 +0300

    init commit

---
+++ Innowise.Clinic.Documents.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,8 @@
+using Innowise.Clinic.Shared.ControllersAbstractions;
+
+namespace Innowise.Clinic.Documents.Api.Controllers;
+
+public class WeatherForecastController : ApiControllerBase
+{
+    
+}
